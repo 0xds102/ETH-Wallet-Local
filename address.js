90fd72ecc88ce10ff8a0b7dc2ab5a1de4c7a2b06
Suspect <84794559+suspect0x@users.noreply.github.com>
2022-01-18 22:29:47 -0600

    Update address.js

--- address.js
+++ address.js
@@ -1,12 +1,12 @@
 // Ethers.js
 // Creates a private & public key for an Ethereum wallet address
 
-var ethers = require('ethers');  
-var crypto = require('crypto');
+const ethers = require('ethers');  
+const crypto = require('crypto');
 
-var id = crypto.randomBytes(32).toString('hex');
-var privateKey = "0x"+id;
+const id = crypto.randomBytes(32).toString('hex');
+const privateKey = "0x"+id;
 console.log("Private Key:", privateKey);
 
-var wallet = new ethers.Wallet(privateKey);
+const wallet = new ethers.Wallet(privateKey);
 console.log("Address: " + wallet.address);
